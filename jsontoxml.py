75868077c1c856c66291273c15da16678736b97e
aprila14 <36075569+aprila14@users.noreply.github.com>
2020-11-08 20:06:44 +0100

    change name to text

--- jsontoxml.py
+++ jsontoxml.py
@@ -1,42 +1,39 @@
 import json
 import xml.etree.ElementTree as ET
 
-
+#define the training image size
 imgwidth = 535
 imgheight = 414
+numberofimages = 0
 
-
+#open the json file with the data
 with open('C:/Users/u380564/OneDrive/Dokumente/objectDetection/Code/objectdetection/tensorflow1/models/research/object_detection/images/train/captures_000.json') as f:
   data = json.load(f)
-numberofimages = 0
-# Output: {'name': 'Bob', 'languages': ['English', 'Fench']}
 
-for i in data["captures"]: ##iterate through all the images
-    
+#iterate through all the images
+for i in data["captures"]:
     numberofimages = numberofimages + 1
     filenamename = i["filename"]
+    
+    #define the the path in which the training images are
     pathname = "C:/Users/u380564/OneDrive/Dokumente/objectDetection/Artificial Dataset/ea391af9-d56c-458b-bc5e-8ac55cbb47e6"
     pathname = pathname+filenamename
-    
     numberofobjects = len(i['annotations'][0]["values"])
     
     # create the file structure
     annotation = ET.Element('annotation')
-    
     folder = ET.SubElement(annotation, 'folder')
-    folder.set('name','train')
-    folder.text=''
+    folder.text='train'
     filename = ET.SubElement(annotation,'filename')
-    filename.set('name',filenamename)
-    filename.text=""
+    filename.text=filenamename
     path = ET.SubElement(annotation, 'path')
-    path.set('name',pathname)
+    path.text = pathname
     source = ET.SubElement(annotation, 'source')
-    source.set('name','source')
+    source.text = 'source'
     database = ET.SubElement(source, 'database')
-    database.set('name','Unknown')
+    database.text = 'Unknown'
     
-
+    #iterate through all the object in 1 image
     for n in range(numberofobjects):
         ##print((i['annotations'][0]))
         widthvalue = i['annotations'][0]['values'][n]['width']
@@ -44,13 +41,13 @@ for i in data["captures"]: ##iterate through all the images
 
         size = ET.SubElement(annotation, 'size')
         width = ET.SubElement(size, 'width')
-        width.set('name', str(imgwidth))
+        width.text = str(imgwidth)
         height = ET.SubElement(size, 'height')
-        height.set('name', str(imgheight))
+        height.text = str(imgheight)
         depth = ET.SubElement(size, 'depth')
-        depth.set('name','3')
+        depth.text = '3'
         segmented = ET.SubElement(annotation, 'segmented')
-        segmented.set("name", '0')
+        segmented.text = '0'
 
         namename = i['annotations'][0]['values'][n]['label_name']
         Xmin = i['annotations'][0]['values'][n]['x']
@@ -61,28 +58,34 @@ for i in data["captures"]: ##iterate through all the images
 
         object = ET.SubElement(annotation, 'object')
         name = ET.SubElement(object, 'name')
-        name.set('name',namename)
+        name.text = namename
         pose = ET.SubElement(object, 'pose')
-        pose.set('name','Unspecified')
+        pose.text = 'Unspecified'
         truncate = ET.SubElement(object, 'truncate')
-        truncate.set("name",'0')
+        truncate.text = '0'
         difficult = ET.SubElement(object, 'difficult')
-        difficult.set('name','difficult')
+        difficult.text = 'difficult'
         bndbox = ET.SubElement(object, 'bndbox')
-        bndbox.set('name',"bndbox")
+        bndbox.text = 'bndbox'
         xmin = ET.SubElement(bndbox, 'xmin')
-        xmin.set('name',str(Xmin))
+        xmin.text = str(Xmin)
         ymin = ET.SubElement(bndbox, 'ymin')
-        ymin.set('name',str(Ymin))
+        ymin.text = str(Ymin)
         xmax = ET.SubElement(bndbox, 'xmax')
-        xmax.set('name',str(Xmax))
+        xmax.text = str(Xmax)
         ymax = ET.SubElement(bndbox, 'ymax')
-        ymax.set('name',str(Ymax))
+        ymax.text = str(Ymax)
         
         # create a new XML file with the results
         mydata = ET.tostring(annotation)
+        
+        #create output filename
+        imgname = filenamename.split("/")
+        imgnameending = imgname[1]
+        outputfilename = imgnameending[:-3] + "xml"
+        print(outputfilename)
         # Save file
-        myfile = open("caputures_000.xml", "wb")
+        myfile = open(outputfilename, "wb")
         myfile.write(mydata)
         
 
